23477fdac54462cb2326d7cda810401c462d6d2d
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-09-25 22:03:09 +0200

    bug fixed

--- netlify/functions/getAllTask.js
+++ netlify/functions/getAllTask.js
@@ -12,13 +12,13 @@ exports.handler = async (event, context) => {
     const tasks = await Todo.find();
     console.log("Fetched Tasks", tasks);
 
-    if (tasks.length === 0) {
+    /*  if (tasks.length === 0) {
       return {
         statusCode: 404,
         body: JSON.stringify({ message: "No Tasks Found" }),
       };
     }
-
+ */
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Tasks Found", tasks }),
