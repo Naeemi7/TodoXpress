aff9b6386dfd5a1034159c11a14f4e88210bdef1
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-09-25 14:25:54 +0200

    bug fixed

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Creating a new Axios instance with a default base URL and headers
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "/.netlify/functions",
   headers: {
     "Content-Type": "application/json",
   },
