8969c82bbc5f12272e23d827b052cb79dcda041a
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-05 00:52:46 +0200

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,12 @@ const port = process.env.PORT || 3000;
 // Parse the JSON body to req.body
 app.use(express.json());
 
-/* const corsOption = {
+const corsOption = {
   origin: "*", // You might want to restrict this to specific origins in a production environment.
   methods: ["HEAD", "GET", "POST", "PATCH", "DELETE", "PUT"],
 };
 
-app.use(cors(corsOption)); */
-
-app.use(
-  cors({
-    origin: ["https://depoly-mern01whq.vercel.app"],
-    method: ["POST", "PATCH", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors(corsOption));
 
 // Connecting to MongoDB
 mongoose
